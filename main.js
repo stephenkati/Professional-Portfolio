7970979a5d89b73c1921b3d212d6f981b0ef6615
stephenkati <stephenkatuli88@gmail.com>
2023-03-11 08:31:15 +0300

    Add mobile menu functionality

---
+++ main.js
@@ -0,0 +1,20 @@
+const openMenu = document.querySelector('#menu');
+const closeMenu = document.querySelector('#close');
+const mobileMenu = document.querySelector('#mobile');
+const menuLinks = document.querySelectorAll('.mobileMenu');
+
+openMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  mobileMenu.style.display = 'flex';
+});
+
+closeMenu.addEventListener('click', (e) => {
+  e.preventDefault();
+  mobileMenu.style.display = 'none';
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  })
+})
